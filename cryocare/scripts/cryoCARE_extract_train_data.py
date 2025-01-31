731f59fe2c21bb2facd246ce895d7e6f678b0af5
DanGonite57 <13329420+DanGonite57@users.noreply.github.com>
2025-01-31 08:46:27 +0000

    read all slices from motioncor folder

--- cryocare/scripts/cryoCARE_extract_train_data.py
+++ cryocare/scripts/cryoCARE_extract_train_data.py
@@ -4,6 +4,7 @@ import json
 import warnings
 import os
 import sys
+import random
 from cryocare.internals.CryoCAREDataModule import CryoCARE_DataModule
 
 
@@ -23,8 +24,15 @@ def main():
     with open(args.conf, 'r') as f:
         config = json.load(f)
 
+    if config.get("folders"):
+        odd = []
+        for folder in config["folders"]:
+            odd += [os.path.join(folder, x) for x in os.listdir(folder) if "_ODD.mrc" in x]
+        config["odd"] = random.sample(odd, config["num_slices"])
+        config["even"] = [x.replace("_ODD.mrc", "_EVN.mrc") for x in config["odd"]]
+
     dm = CryoCARE_DataModule()
-    dm.setup(config['odd'], config['even'], mask_paths=config['mask'] if 'mask' in config else None, n_samples_per_tomo=config['num_slices'],
+    dm.setup(config['odd'], config['even'], mask_paths=config['mask'] if 'mask' in config else None, n_samples_per_tomo=config['num_samples'],
                              validation_fraction=(1.0 - config['split']), sample_shape=config['patch_shape'],
                              tilt_axis=config['tilt_axis'], n_normalization_samples=config['n_normalization_samples'])
     
